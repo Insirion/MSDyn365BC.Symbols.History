973634e7797a80196c1ae3876e69473c972a6128
Jan Hoek <github@uncommonsense.nl>
2024-01-09 19:21:49 +0100

    Use different destination dirs

--- test.ps1
+++ test.ps1
@@ -1,9 +1,9 @@
 Get-BCSymbols `
     -ArtifactUrl https://bcartifacts.azureedge.net/onprem/23.2.14098.14562/nl `
-    -Directory ~/Desktop `
+    -Directory ~/Desktop/onprem `
     -Verbose
 
 Get-BCSymbols `
     -ArtifactUrl https://bcartifacts.azureedge.net/sandbox/23.2.14098.15193/nl `
-    -Directory ~/Desktop `
+    -Directory ~/Desktop/sandbox `
     -Verbose
