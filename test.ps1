13718e412dc97229a18dca377df19ba98ffeaaf4
Jan Hoek <github@uncommonsense.nl>
2024-01-09 18:11:58 +0100

    Added sandbox url to test.ps1

--- test.ps1
+++ test.ps1
@@ -2,3 +2,8 @@ Get-BCSymbols `
     -ArtifactUrl https://bcartifacts.azureedge.net/onprem/23.2.14098.14562/nl `
     -Directory ~/Desktop `
     -Verbose
+
+Get-BCSymbols `
+    -ArtifactUrl https://bcartifacts.azureedge.net/sandbox/23.2.14098.15193/nl `
+    -Directory ~/Desktop `
+    -Verbose
