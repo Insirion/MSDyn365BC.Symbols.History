35caaefe7e0e41bdb64ffb431ec1a8a3507f4b94
Jan Hoek <github@uncommonsense.nl>
2024-02-27 15:07:58 +0100

    Preparations

--- public/Get-BCSymbols.ps1
+++ public/Get-BCSymbols.ps1
@@ -50,7 +50,6 @@ function Get-BCSymbols
 
     Expand-FileFromZipArchive `
         -Uri $PlatformUrl `
-        -CentralDirSize 1mb `
         -ZipEntryPath 'ModernDev\program files\Microsoft Dynamics NAV\230\AL Development Environment\System.app' `
         -Destination $Directory `
         -NoContainer
--- test.ps1
+++ test.ps1
@@ -1,3 +1,5 @@
+Get-Module -ListAvailable UncommonSense.Zip.Utils | Import-Module -Force
+
 Get-BCSymbols `
     -ArtifactUrl https://bcartifacts.azureedge.net/onprem/23.2.14098.14562/nl `
     -Directory ~/Desktop/onprem `
