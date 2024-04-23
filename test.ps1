c5db826598899afa5826ca4488d1f67f2ab896e7
Jan Hoek <github@uncommonsense.nl>
2024-04-23 18:33:34 +0200

    Updated artifact urls

--- test.ps1
+++ test.ps1
@@ -1,11 +1,13 @@
+$ErrorActionPreference = 'Stop'
+
 Get-Module -ListAvailable UncommonSense.Zip.Utils | Import-Module -Force
 
 Get-BCSymbols `
-    -ArtifactUrl https://bcartifacts.azureedge.net/onprem/23.2.14098.14562/nl `
+    -ArtifactUrl 'https://bcartifacts-exdbf9fwegejdqak.b02.azurefd.net/onprem/24.0.16410.18056/nl' `
     -Directory ~/Desktop/onprem `
     -Verbose
 
 Get-BCSymbols `
-    -ArtifactUrl https://bcartifacts.azureedge.net/sandbox/23.2.14098.15193/nl `
+    -ArtifactUrl 'https://bcartifacts-exdbf9fwegejdqak.b02.azurefd.net/sandbox/24.0.16410.18817/nl' `
     -Directory ~/Desktop/sandbox `
     -Verbose
